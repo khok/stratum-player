674d80da6e1946e1516e271c01b60b9857acbc33
Maxim Khokhryakov <khokhryakovmax@yandex.ru>
2019-09-25 16:07:46 +0500

    Добален код композиции схемы

--- src/core/project.ts
+++ src/core/project.ts
@@ -1,12 +1,57 @@
 import { StratumError } from "../errors";
+import { insertImage, replaceIcon } from "../graphics/schemeComposer";
+import { HandleMap, VectorDrawData } from "../graphics/types";
 import { VectorDrawInstance } from "../graphics/vectorDrawInstance";
 import { WindowSystem } from "../graphics/windowSystem";
 import { createDefaultValue } from "../helpers";
-import { ClassData, VarSet } from "../types";
+import { ChildData, ClassData, VarSet } from "../types";
 import { ClassFunctions, GraphicSpaceResolver, ProjectFunctions, VmBool } from "../vm/types";
 import { VirtualMachine } from "../vm/virtualMachine";
 import { ChildFactory, Variable } from "./classBase";
 import { ClassInstance } from "./classInstance";
+
+function copyMap<T>(map: HandleMap<T>) {
+    const newMap = new Map<number, T>();
+    map.forEach((v, k) => newMap.set(k, v));
+    return newMap;
+}
+
+function makeImageCopy(image: VectorDrawData) {
+    const imageCopy = { ...image };
+    if (image.elements) imageCopy.elements = copyMap(image.elements);
+    if (image.texts) imageCopy.texts = copyMap(image.texts);
+    return imageCopy;
+}
+
+function recalcImageOrigin(image: VectorDrawData) {
+    let x: number | undefined = undefined,
+        y: number | undefined = undefined;
+    if (!image.elements) return;
+    for (const el of image.elements.values()) {
+        if (el.type == "group") continue;
+        if (x == undefined || el.position.x < x) x = el.position.x;
+        if (y == undefined || el.position.y < y) y = el.position.y;
+    }
+    image.origin = { x: x || 0, y: y || 0 };
+}
+
+function composeScheme(scheme: VectorDrawData, childs: ChildData[], classes: Map<string, ClassData>) {
+    for (const c of childs) {
+        const childProto = classes.get(c.className)!;
+        const { handle: groupHandle, position } = c.onSchemeData;
+        if (childProto.image) {
+            const image = childProto.image as VectorDrawData & { __originRecalculated?: boolean };
+            if (!image.__originRecalculated) {
+                recalcImageOrigin(image);
+                image.__originRecalculated = true;
+            }
+            insertImage(scheme, makeImageCopy(childProto.image), groupHandle, position);
+        } else {
+            const { iconRef, iconIndex } = childProto;
+            if (iconRef) replaceIcon(scheme, groupHandle, iconRef, iconIndex || 0);
+        }
+    }
+}
 
 export class Project implements ProjectFunctions {
     private tree: ClassInstance;
@@ -35,11 +80,11 @@ export class Project implements ProjectFunctions {
     createSchemeInstance(className: string): GraphicSpaceResolver | undefined {
         const proto = this.classCollection.get(className);
         if (!proto || !proto.scheme) return undefined;
-        const { scheme } = proto;
-        // if (!scheme.composed) {
-        //     if (proto.childs) composeScheme(scheme, this.classCollection, proto.childs);
-        //     (<MutableStratumScheme>scheme).composed = true;
-        // }
+        const scheme = proto.scheme as VectorDrawData & { __composed?: boolean };
+        if (!scheme.__composed) {
+            if (proto.childs) composeScheme(scheme, proto.childs, this.classCollection);
+            scheme.__composed = true;
+        }
         return canvas => new VectorDrawInstance(scheme, canvas);
     }
     hasClass(className: string): VmBool {
