f9a6b1060436aaf32f92feef6c61900cd1c28e14
Maxim Khokhryakov <khokm@ya.ru>
2020-03-04 21:20:02 +0500

    добавлен маленький тест

---
+++ src/tests/manual/test-show-scheme.ts
@@ -0,0 +1,25 @@
+import { loadProjectData, openZipFromUrl, ReadOptions } from "~/fileReader/fileReaderHelpers";
+import { GraphicSpace } from "~/graphics/graphicSpace/graphicSpace";
+import { SimpleImageLoader } from "~/graphics/graphicSpace/simpleImageLoader";
+import { FabricScene } from "~/graphics/renderers/fabricRenderer/fabricScene";
+import { createComposedScheme } from "~/helpers/graphics";
+import { HtmlFactory } from "~/helpers/htmlFactory";
+
+//Запуск проекта `name` с использованием api.ts
+export async function _show_scheme(name: string, opts: ReadOptions) {
+    const zipFiles = await openZipFromUrl([`test_projects/${name}.zip`, "/data/library.zip"]);
+    const { collection, rootName } = await loadProjectData(zipFiles, opts);
+
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    const htmlRoot = document.getElementById("root")!;
+
+    const root = collection.get(rootName)!;
+    const scheme = createComposedScheme(root.scheme!, root.childs!, collection);
+
+    const inputFactory = new HtmlFactory(htmlRoot);
+    const scene = new FabricScene({ canvas, inputFactory, view: scheme.origin });
+    const imageResolver = new SimpleImageLoader("data/icons");
+    GraphicSpace.fromVdr("", scheme, imageResolver, scene);
+    setTimeout(() => scene.render(), 100);
+    // scene.render();
+}
