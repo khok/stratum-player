9f52859b7c4608196c41b1734afef60ac714e3b6
Maxim Khokhryakov <khokm@ya.ru>
2020-04-27 01:24:44 +0500

    update test

--- src/tests/manual/test-run-quick.ts
+++ src/tests/manual/test-run-quick.ts
@@ -1,9 +1,14 @@
 import { fromUrl, ExtendedPlayerOptions } from "~/api";
+import { showMissingCommands, formatMissingCommands } from "~/helpers/showMissingCommands";
+import { VmOperations } from "~/vm/operations";
+import { Project } from "~/core/project";
 
 //Запуск проекта `name` с использованием api.ts
 export async function _run_test_quick(name: string, opts: ExtendedPlayerOptions, timeout?: number) {
     const player = await fromUrl([`test_projects/${name}.zip`, "/data/library.zip"], opts);
     console.dir(player);
+    const mc = showMissingCommands((player!["vm"]["project"] as Project)["classesData"], VmOperations);
+    console.log(formatMissingCommands(mc.missingOperations));
     if (!player) return;
     const globalCanvas = document.getElementById("canvas") as HTMLCanvasElement;
     const htmlRoot = document.getElementById("root")!;
