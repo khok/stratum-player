d4a7873cd64f51388eec70a78a76aaeb40a7c708
Maxim Khokhryakov <khokm@ya.ru>
2021-03-18 03:52:46 +0500

    Добавлены проекты

--- examples/loader.js
+++ examples/loader.js
@@ -1,19 +1,23 @@
-const runProject = (name) => {
+const runProject = (name, fast, path) => {
     stratum.options.iconsLocation = "../data/icons";
     const urls = [`./${name}.zip`, "../data/library.zip"];
     const promises = urls.map(url => fetch(url).then(res => res.blob()).then(stratum.unzip))
     Promise.all(promises)
     .then(async (fsArr) => {
         const fs = fsArr.reduce((a, b) => a.merge(b));
-        if(document.body) document.body.innerHTML = "Подзагруажем BMP и VDR файлы..."
-        await Promise.all([...fs.files(/.+\.(bmp|vdr)$/i)].map((f) => f.makeSync()))
+        if(document.body) document.body.innerHTML = "Подзагруажем файлы..."
+        await Promise.all([...fs.files(/.+\.(bmp|vdr|dbm|txt|mat)$/i)].map((f) => f.makeSync()))
         if(document.body) document.body.innerHTML = "Загружаем ресурсы проекта..."
-        return fs.project({ additionalClassPaths: ["library"] });
+        return fs.project({ path, additionalClassPaths: ["library"] });
     })
     .then((prj) => {
         const cb = () => {
+            prj.computer = new stratum[fast ? "FastestExecutor" : "SmoothExecutor"]();
             document.body.innerHTML = "";
-            prj.on("closed", () => history.back()).play(document.body);
+            prj
+            .on("closed", () => history.back())
+            .on("error", (err) => alert(err))
+            .play(document.body);
         }
         if(document.body) cb();
         else window.addEventListener("load", cb);
